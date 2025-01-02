025be717f174412ac0ab4cca2551bb21c69f342f
炫炫 <2066855608@qq.com>
2025-01-02 16:30:48 +0800

    细节优化

--- src/module/utils/FFmpeg.ts
+++ src/module/utils/FFmpeg.ts
@@ -95,15 +95,15 @@ class FFmpeg {
   async FFmpeg<T extends keyof fffmpegClientOptions> (opt: any): Promise<MergeFileResult<T>> {
     switch (this.type) {
       case '二合一（视频 + 音频）': {
-        const result = await ffmpeg(`-y -i ${opt.path} -i ${opt.path2} -c copy ${opt.resultPath}`, { booleanResult: true })
-        result ? logger.mark('视频合成成功！') : logger.error('视频合成失败！')
-        await opt.callback(result, opt.resultPath)
+        const result = await ffmpeg(`-y -i ${opt.path} -i ${opt.path2} -c copy ${opt.resultPath}`)
+        result.status ? logger.mark('视频合成成功！') : logger.error(result)
+        await opt.callback(result.status, opt.resultPath)
         return result as unknown as MergeFileResult<T> // 布尔类型
       }
       case '视频*3 + 音频': {
-        const result = await ffmpeg(`-y -stream_loop 2 -i ${opt.path} -i ${opt.path2} -filter_complex "[0:v]setpts=N/FRAME_RATE/TB[v];[0:a][1:a]amix=inputs=2:duration=shortest:dropout_transition=3[aout]" -map "[v]" -map "[aout]" -c:v libx264 -c:a aac -b:a 192k -shortest ${opt.resultPath}`, { booleanResult: true })
-        result ? logger.mark('视频合成成功！') : logger.error('视频合成失败！')
-        await opt.callback(result, opt.resultPath)
+        const result = await ffmpeg(`-y -stream_loop 2 -i ${opt.path} -i ${opt.path2} -filter_complex "[0:v]setpts=N/FRAME_RATE/TB[v];[0:a][1:a]amix=inputs=2:duration=shortest:dropout_transition=3[aout]" -map "[v]" -map "[aout]" -c:v libx264 -c:a aac -b:a 192k -shortest ${opt.resultPath}`)
+        result ? logger.mark('视频合成成功！') : logger.error(result)
+        await opt.callback(result.status, opt.resultPath)
         return result as unknown as MergeFileResult<T> // 布尔类型
       }
       case '获取指定视频文件时长': {
@@ -111,11 +111,14 @@ class FFmpeg {
         return parseFloat(parseFloat(stdout.trim()).toFixed(2)) as unknown as MergeFileResult<T>  // 数字类型
       }
       case '压缩视频': {
-        const result = await ffmpeg(`-y -i "${opt.path}" -b:v ${opt.targetBitrate}k -maxrate ${opt.maxRate || opt.targetBitrate * 1.5}k -bufsize ${opt.bufSize || opt.targetBitrate * 2}k -crf ${opt.crf || 35} -preset medium -c:v libx264 -vf "scale='if(gte(iw/ih,16/9),1280,-1)':'if(gte(iw/ih,16/9),-1,720)',scale=ceil(iw/2)*2:ceil(ih/2)*2" "${opt.resultPath}"`, { booleanResult: true })
-        if (result) {
+        const result = await ffmpeg(`-y -i "${opt.path}" -b:v ${opt.targetBitrate}k -maxrate ${opt.maxRate || opt.targetBitrate * 1.5}k -bufsize ${opt.bufSize || opt.targetBitrate * 2}k -crf ${opt.crf || 35} -preset medium -c:v libx264 -vf "scale='if(gte(iw/ih,16/9),1280,-1)':'if(gte(iw/ih,16/9),-1,720)',scale=ceil(iw/2)*2:ceil(ih/2)*2" "${opt.resultPath}"`)
+        if (result.status) {
           logger.mark(`视频已压缩并保存到: ${opt.resultPath}`)
           Common.removeFile(opt.path)
-        } else logger.error(opt.path + ' 压缩失败！')
+        } else {
+          logger.error(opt.path + ' 压缩失败！')
+          logger.error(result)
+        }
         return opt.resultPath as unknown as MergeFileResult<T>  // 字符串类型
       }
     }
