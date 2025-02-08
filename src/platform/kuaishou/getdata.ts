1b8bf72db279cd07c775fd326fbc45d44cc05706
炫炫 <112480306+ikenxuan@users.noreply.github.com>
2025-02-09 00:46:57 +0800

    fix(platform): 重构部分平台的数据获取和推送逻辑 (#34)
    * fix(platform): 重构部分平台的数据获取和推送逻辑
    
    - 更新了多个平台的API调用方式
    - 优化了数据处理流程，提高了代码的可读性和可维护性
    - 调整了部分变量类型，提高了类型安全性
    
    * 1
    
    * perf: 跟进解析库版本

--- src/platform/kuaishou/getdata.ts
+++ src/platform/kuaishou/getdata.ts
@@ -11,17 +11,17 @@ export async function fetchKuaishouData<T extends keyof KuaishouDataTypes> (
   switch (type) {
     case 'one_work': {
       const VideoData = await client.getKuaishouData('单个视频作品数据', {
-        photoId: (opt as KuaishouDataOptionsMap['单个视频作品数据']).photoId
+        photoId: (opt as KuaishouDataOptionsMap['单个视频作品数据']['opt']).photoId
       })
       const CommentsData = await client.getKuaishouData('评论数据', {
-        photoId: (opt as KuaishouDataOptionsMap['评论数据']).photoId
+        photoId: (opt as KuaishouDataOptionsMap['评论数据']['opt']).photoId
       })
       const EmojiData = await client.getKuaishouData('Emoji数据')
       return { VideoData, CommentsData, EmojiData }
     }
     case 'work_comments': {
       const CommentsData = await client.getKuaishouData('评论数据', {
-        photoId: (opt as KuaishouDataOptionsMap['评论数据']).photoId
+        photoId: (opt as KuaishouDataOptionsMap['评论数据']['opt']).photoId
       })
       return CommentsData
     }
