05e61887430256eeeda668450dfcb5216386a734
炫炫 <2066855608@qq.com>
2025-03-25 03:23:04 +0800

    fix: 统一将 `startText` 替换为 `label` 以提升代码一致性

--- src/web.config.ts
+++ src/web.config.ts
@@ -86,12 +86,12 @@ export default {
           subtitle: '此处用于管理插件的基本设置',
           children: [
             components.switch.create('rmmp4', {
-              startText: '缓存删除',
+              label: '缓存删除',
               description: '缓存自动删除，非必要不修改！',
               defaultSelected: all.app.rmmp4
             }),
             components.switch.create('defaulttool', {
-              startText: '默认解析',
+              label: '默认解析',
               description: '即识别最高优先级，修改后重启生效',
               defaultSelected: all.app.defaulttool
             }),
@@ -114,12 +114,12 @@ export default {
               ]
             }),
             components.switch.create('APIServer', {
-              startText: 'API服务',
+              label: 'API服务',
               description: '本地部署一个视频解析API服务，接口范围为本插件用到的所有',
               defaultSelected: all.app.APIServer
             }),
             components.switch.create('APIServerMount', {
-              startText: '挂载到 Karin',
+              label: '挂载到 Karin',
               description: 'API 服务是否挂载到 Karin 上，开启后监听端口为 Karin 的 http 端口，修改后需重启',
               defaultSelected: all.app.APIServerMount
             }),
@@ -155,7 +155,7 @@ export default {
               ]
             }),
             components.switch.create('RemoveWatermark', {
-              startText: '移除水印',
+              label: '移除水印',
               description: '渲染的图片是否移除底部水印',
               defaultSelected: all.app.RemoveWatermark
             })
@@ -172,17 +172,17 @@ export default {
           subtitle: '此处为抖音相关的用户偏好设置',
           children: [
             components.switch.create('switch', {
-              startText: '解析开关',
+              label: '解析开关',
               description: '抖音解析开关，此开关为单独开关',
               defaultSelected: all.douyin.switch
             }),
             components.switch.create('tip', {
-              startText: '解析提示',
+              label: '解析提示',
               description: '抖音解析提示，发送提示信息：“检测到抖音链接，开始解析”',
               defaultSelected: all.douyin.tip
             }),
             components.switch.create('comment', {
-              startText: '评论解析',
+              label: '评论解析',
               description: '抖音评论解析，开启后可发送抖音作品评论图',
               defaultSelected: all.douyin.comment
             }),
@@ -192,7 +192,7 @@ export default {
               rules: [{ min: 1 }]
             }),
             components.switch.create('autoResolution', {
-              startText: '自动分辨率',
+              label: '自动分辨率',
               description: '根据「视频拦截阈值」自动选择合适的分辨率，关闭后默认选择最大分辨率进行下载',
               defaultSelected: all.douyin.autoResolution
             }),
@@ -201,7 +201,7 @@ export default {
               descPosition: 20
             }),
             components.switch.create('push:switch', {
-              startText: '推送开关',
+              label: '推送开关',
               description: '推送开关，开启后需重启；使用「#设置抖音推送 + 抖音号」配置推送列表',
               defaultSelected: all.douyin.push.switch
             }),
@@ -259,12 +259,12 @@ export default {
               defaultValue: all.douyin.push.cron
             }),
             components.switch.create('push:parsedynamic', {
-              startText: '作品解析',
+              label: '作品解析',
               description: '触发推送时是否一同解析该作品',
               defaultSelected: all.douyin.push.parsedynamic
             }),
             components.switch.create('push:log', {
-              startText: '推送日志',
+              label: '推送日志',
               description: '是否打印推送日志（修改后需重启）',
               defaultSelected: all.douyin.push.log
             }),
@@ -296,17 +296,17 @@ export default {
           subtitle: '此处为B站相关的用户偏好设置',
           children: [
             components.switch.create('switch', {
-              startText: '解析开关',
+              label: '解析开关',
               description: 'B站解析开关，此开关为单独开关',
               defaultSelected: all.bilibili.switch
             }),
             components.switch.create('tip', {
-              startText: '解析提示',
+              label: '解析提示',
               description: 'B站解析提示，发送提示信息：“检测到B站链接，开始解析”',
               defaultSelected: all.bilibili.tip
             }),
             components.switch.create('comment', {
-              startText: '评论解析',
+              label: '评论解析',
               description: 'B站评论解析，开启后可发送B站作品评论图',
               defaultSelected: all.bilibili.comment
             }),
@@ -316,12 +316,12 @@ export default {
               rules: [{ min: 1 }]
             }),
             components.switch.create('videopriority', {
-              startText: '优先保内容',
+              label: '优先保内容',
               description: '解析视频是否优先保内容，true为优先保证上传将使用最低分辨率，false为优先保清晰度将使用最高分辨率',
               defaultSelected: all.bilibili.videopriority
             }),
             components.switch.create('autoResolution', {
-              startText: '自动分辨率',
+              label: '自动分辨率',
               description: '根据「视频拦截阈值」自动选择合适的分辨率，关闭后默认选择最大分辨率进行下载',
               defaultSelected: all.bilibili.autoResolution
             }),
@@ -330,7 +330,7 @@ export default {
               descPosition: 20
             }),
             components.switch.create('push:switch', {
-              startText: '推送开关',
+              label: '推送开关',
               description: '推送开关，开启后需重启；使用「#设置B站推送 + UID」配置推送列表',
               defaultSelected: all.bilibili.push.switch
             }),
@@ -388,12 +388,12 @@ export default {
               defaultValue: all.bilibili.push.cron
             }),
             components.switch.create('push:parsedynamic', {
-              startText: '作品解析',
+              label: '作品解析',
               description: '触发推送时是否一同解析该作品',
               defaultSelected: all.bilibili.push.parsedynamic
             }),
             components.switch.create('push:log', {
-              startText: '推送日志',
+              label: '推送日志',
               description: '是否打印推送日志（修改后需重启）',
               defaultSelected: all.bilibili.push.log
             })
@@ -410,17 +410,17 @@ export default {
           subtitle: '此处为快手相关的用户偏好设置',
           children: [
             components.switch.create('switch', {
-              startText: '解析开关',
+              label: '解析开关',
               description: '快手解析开关，此开关为单独开关',
               defaultSelected: all.kuaishou.switch
             }),
             components.switch.create('tip', {
-              startText: '解析提示',
+              label: '解析提示',
               description: '抖音解析提示，发送提示信息：“检测到快手链接，开始解析”',
               defaultSelected: all.kuaishou.tip
             }),
             components.switch.create('comment', {
-              startText: '评论解析',
+              label: '评论解析',
               description: '快手评论解析，开启后可发送抖音作品评论图',
               defaultSelected: all.kuaishou.comment
             }),
@@ -442,12 +442,12 @@ export default {
           subtitle: '此处为上传相关的用户偏好设置',
           children: [
             components.switch.create('sendbase64', {
-              startText: '转换Base64',
+              label: '转换Base64',
               description: '发送视频经本插件转换为base64格式后再发送，适合Karin与机器人不在同一网络环境下开启',
               defaultSelected: all.upload.swisendbase64tch
             }),
             components.switch.create('usefilelimit', {
-              startText: '视频上传拦截',
+              label: '视频上传拦截',
               description: '开启后会根据视频文件大小判断是否需要上传，需配置「视频拦截阈值」。',
               defaultSelected: all.upload.usefilelimit
             }),
@@ -459,7 +459,7 @@ export default {
               isDisabled: !all.upload.usefilelimit
             }),
             components.switch.create('compress', {
-              startText: '压缩视频',
+              label: '压缩视频',
               description: '开启后会将视频文件压缩后再上传，适合上传大文件，任务过程中会吃满CPU，对低配服务器不友好。需配置「压缩触发阈值」与「压缩后的值」',
               defaultSelected: all.upload.compress
             }),
@@ -478,7 +478,7 @@ export default {
               isDisabled: !all.upload.compress
             }),
             components.switch.create('usegroupfile', {
-              startText: '群文件上传',
+              label: '群文件上传',
               description: '使用群文件上传，开启后会将视频文件上传到群文件中，需配置「群文件上传阈值」',
               defaultSelected: all.upload.usegroupfile
             }),
