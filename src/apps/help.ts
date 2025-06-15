88115f108d09620885491dd990a8306bf586f096
炫炫 <2066855608@qq.com>
2025-06-16 02:14:33 +0800

    fix: 更新md-html依赖并优化版本信息展示
    升级@karinjs/md-html至1.3.0版本，同时调整版本信息展示样式，增加字体设置和显示条目数量

--- src/apps/help.ts
+++ src/apps/help.ts
@@ -14,10 +14,15 @@ export const help = karin.command(/^#?kkk帮助$/, async (e) => {
 
 export const version = karin.command(/^#?kkk(版本|更新日志)$/, async (e) => {
   const changelogContent = fs.readFileSync(Root.pluginPath + '/CHANGELOG.md', 'utf8')
-  const forwardLogs = logs(Root.pluginVersion, changelogContent, 8, false)
+  const forwardLogs = logs(Root.pluginVersion, changelogContent, 10, false)
 
   const html = markdown(forwardLogs, {
-    gitcss: Common.useDarkTheme() ? 'github-markdown-dark.css' : 'github-markdown-light.css'
+    gitcss: Common.useDarkTheme() ? 'github-markdown-dark.css' : 'github-markdown-light.css',
+    scale: 5,
+    customCSSFiles: [
+      Root.pluginPath + '/resources/font/font.css'
+    ],
+    fontFamily: 'HarmonyOSHans-Regular'
   })
   mkdirSync(`${karinPathTemp}/html/${Root.pluginName}/version`)
   const htmlPath = `${karinPathTemp}/html/${Root.pluginName}/version/version.html`
