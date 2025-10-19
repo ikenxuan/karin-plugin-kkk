dcac9bda8e19220c5edcc0f1d831ec23d44c69b5
炫炫 <2066855608@qq.com>
2025-10-19 15:18:31 +0800

    config: 默认配置都为最高请

--- packages/core/src/types/config/xiaohongshu.ts
+++ packages/core/src/types/config/xiaohongshu.ts
@@ -8,7 +8,14 @@ export interface xiaohongshuConfig {
   sendContent: ('info' | 'comment' | 'image' | 'video')[]
   /** 小红书评论数量 */
   numcomment: number
-  /** 视频画质偏好设置，'adapt' 为自动根据「maxAutoVideoSize」大小选择，其他为固定画质 */
+  /**
+   * 视频画质偏好设置，'adapt' 为自动根据「maxAutoVideoSize」大小选择，其他为固定画质
+   * - '540p': 540P 标清
+   * - '720p': 720P 高清
+   * - '1080p': 1080P 超清
+   * - '2k': 2K 超高清
+   * - '4k': 4K 超高清
+   */
   videoQuality: 'adapt' | '540p' | '720p' | '1080p' | '2k' | '4k'
   /** 视频体积上限，自动画质模式下可接受的最大视频大小（单位：MB），仅在 「videoQuality」 为 'adapt' 时生效 */
   maxAutoVideoSize: number
