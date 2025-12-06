d82fe01a9318038d273f189b14655d11f4bf8d9e
炫炫 <2066855608@qq.com>
2025-12-06 23:07:44 +0800

    docs: preview

--- packages/docs/react-router.config.ts
+++ packages/docs/react-router.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from '@react-router/dev/config';
 import { glob } from 'node:fs/promises';
 import { createGetUrl, getSlugs } from 'fumadocs-core/source';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 const getUrl = createGetUrl('/docs');
 
 export default {
   ssr: true,
+  presets: [vercelPreset()],
   async prerender ({ getStaticPaths }) {
     const paths: string[] = [...getStaticPaths()]
     for await (const entry of glob('**/*.mdx', { cwd: 'content/docs' })) {
