88ed26b1d573ed82af92a78547c51d391c61a7c3
炫炫 <2066855608@qq.com>
2025-12-07 22:17:16 +0800

    docs: 1

--- packages/docs/open-next.config.ts
+++
@@ -1,8 +0,0 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
-
-export default defineCloudflareConfig({
-	incrementalCache: staticAssetsIncrementalCache,
-  enableCacheInterception: true,
-});
