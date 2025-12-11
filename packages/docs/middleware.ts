00e547d3595bfa63d198d900ed05ce2fbbd3d638
炫炫 <2066855608@qq.com>
2025-12-12 02:18:37 +0800

    docs: matcher

--- packages/docs/middleware.ts
+++ packages/docs/middleware.ts
@@ -1,13 +1,13 @@
-import { createI18nMiddleware } from 'fumadocs-core/i18n/middleware';
-import { i18n } from '@/lib/i18n';
+import { createI18nMiddleware } from 'fumadocs-core/i18n/middleware'
+import { i18n } from '@/lib/i18n'
 
 /**
  * Middleware for locale redirection
  * 处理语言重定向的中间件
  */
-export default createI18nMiddleware(i18n);
+export default createI18nMiddleware(i18n)
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+}
