4c0027755c03c1cd39a6278dae07bbca550548fe
炫炫 <2066855608@qq.com>
2026-01-17 03:37:46 +0800

    opt: 1

--- packages/core/src/platform/douyin/push.ts
+++ packages/core/src/platform/douyin/push.ts
@@ -184,8 +184,7 @@ export class DouYinpush extends Base {
             avater_url: 'https://p3-pc.douyinpic.com/aweme/1080x1080/' + Detail_Data.user_info.data.user.avatar_larger.uri,
             fans: this.count(Detail_Data.user_info.data.user.follower_count),
             share_url: 'https://live.douyin.com/' + Detail_Data.room_data.owner.web_rid,
-            dynamicTYPE: '直播动态推送',
-            useDarkTheme: true
+            dynamicTYPE: '直播动态推送'
           })
         } else {
           // 处理普通作品推送
@@ -252,8 +251,7 @@ export class DouYinpush extends Base {
                 co_creator_nums: teamCount,
                 co_creators
               }
-            })(),
-            useDarkTheme: true
+            })()
           })
         }
       }
@@ -641,8 +639,7 @@ export class DouYinpush extends Base {
       groupInfo: {
         groupId: groupInfo.groupId || '',
         groupName: groupInfo.groupName || ''
-      },
-      useDarkTheme: true
+      }
     })
     await this.e.reply(img)
   }
